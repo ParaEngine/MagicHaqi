b59da4a87a4183c79239574259328c868eaf7583
onedou <onedou@126.com>
2026-06-23 17:21:26 +0800

    feat: update game URL and add message sharing functionality

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -3,11 +3,11 @@ App({
     // 开关：dev 环境下是否强制使用正式环境的 URL
     // true  -> 即使在开发环境也使用正式环境 CDN
     // false -> 开发环境使用本地地址
-    useProdUrlInDev: false,
+    useProdUrlInDev: true,
 
     get gameUrl() {
       const localUrl = 'http://192.168.123.5:5173/MagicHaqi.html'
-      const prodUrl = 'https://keepwork.com/api/raw/maisi/maisi/webgames/MagicHaqi/MagicHaqi.html'
+      const prodUrl = 'https://keepwork.com/api/raw/maisi/maisi/webgames/MagicHaqi/release/MagicHaqi_v1.html'
 
       const isDev = __wxConfig.envVersion === 'develop'
       // 非开发环境始终用正式；开发环境根据开关决定
