bdee018b881eade16483257dacaf4a5b51ca2060
onedou <onedou@126.com>
2026-06-24 11:22:02 +0800

    feat: update game URL, add auto-update functionality, and modify project configuration

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,4 +1,48 @@
 App({
+  onLaunch: function () {
+    this.checkForUpdate()
+  },
+
+  // 强制更新：检测到新版本后下载，下载就绪即强制重启应用
+  checkForUpdate: function () {
+    if (!wx.canIUse('getUpdateManager')) {
+      return
+    }
+
+    const updateManager = wx.getUpdateManager()
+
+    updateManager.onCheckForUpdate(function (res) {
+      // 请求完新版本信息的回调，res.hasUpdate 表示有新版本
+      if (res.hasUpdate) {
+        // 新版本会在后台自动下载
+      }
+    })
+
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '检测到新版本，需要重启后才能继续使用~',
+        showCancel: false,
+        confirmText: '立即重启',
+        success: function (res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    })
+
+    updateManager.onUpdateFailed(function () {
+      // 新版本下载失败，引导用户手动更新
+      wx.showModal({
+        title: '已经有新版本了哟~',
+        content: '新版本已经上线啦，请您删除当前小程序，重新搜索打开哟~',
+        showCancel: false
+      })
+    })
+  },
+
   globalData: {
     // 开关：dev 环境下是否强制使用正式环境的 URL
     // true  -> 即使在开发环境也使用正式环境 CDN
